5d103a4cb12d462ba46bbf221bd74b0134eba8a0
AntoBo <anton.bondarenkooooo@gmail.com>
2022-05-05 14:15:21 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,31 +10,25 @@ import SearchContact from './SearchContact/SearchContact';
 export class App extends Component {
   state = {
     contacts: [
-      // { id: 'id-0', name: 'wer', number: '232459-12-56' },
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-0', name: 'wer', number: '232459-12-56' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
 
   filterContacts() {
-    console.log('filterContacts acts');
     const { filter, contacts } = this.state;
-    // let filteredContacts = [];
-    if (!filter) {
-      //do nothing
-      // return [...contacts];
-      return contacts;
-    } else {
-      //filter with filter
+    if (filter) {
       return [
         ...contacts.filter(contact =>
           contact.name.toLowerCase().includes(filter.toLowerCase())
         ),
       ];
     }
+    return contacts;
   }
 
   addContact = (name, number) => {
